cff619c333dc1e1e12448fc82a7109da8ac814c5
AyushSatyam <07ayushkasera@gmail.com>
2020-01-15 08:41:10 -0800

    Wallet UI, public key and private key implemented

--- blockchain_client/blockchain_client.py
+++ blockchain_client/blockchain_client.py
@@ -1,5 +1,8 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,jsonify
+import Crypto
+import Crypto.Random
+from Crypto.PublicKey import RSA
+import binascii
 class transaction:
 
     def __init__(self, sender_address, sender_private_key,receipient_address,value):
@@ -24,7 +27,15 @@ def view_transaction():
 
 @app.route('/wallet/new')
 def wallet_new():
-    return ''
+    random_gen = Crypto.Random.new().read
+    private_key = RSA.generate(1024, random_gen)
+    public_key = private_key.publickey()
+
+    response = {
+        'private_key': binascii.hexlify(private_key.export_key(format('DER'))).decode('ascii'),
+        'public_key': binascii.hexlify(public_key.export_key(format('DER'))).decode('ascii')
+    }
+    return jsonify(response),200
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
