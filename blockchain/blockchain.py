a3eb5a40ae2b8476d5144a5dd2fff8562eaeba0b
AyushSatyam <07ayushkasera@gmail.com>
2020-01-19 10:01:32 -0800

    Blockchain Mining and Proof of work implemented

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,48 +1,185 @@
-from flask import Flask,render_template,jsonify
+from flask import Flask, request, jsonify, render_template
 from time import time
 from flask_cors import CORS
+from collections import OrderedDict
+import binascii
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA
+from uuid import uuid4
+import json
+import hashlib
+
+MINING_SENDER = 'The Blockchain'
+MINING_REWARD = 10
+MINING_DIFICULTY = 2
+
+
 class Blockchain:
+
     def __init__(self):
         self.transactions = []
         self.chain = []
-        #create the genesis block
-        self.create_block(0,'00')
+        self.nodes = set()
+        self.node_id = str(uuid4()).replace('-', '')
+        # Create the genesis block
+        self.create_block(0, '00')
 
-    def create_block(self,nonce,previous_hash):
-        #Add a block of transaction to the blockchain
-        block = {'block_number': len(self.chain)+1,
+    def create_block(self, nonce, previous_hash):
+        """
+        Add a block of transactions to the blockchain
+        """
+        block = {'block_number': len(self.chain) + 1,
                  'timestamp': time(),
                  'transactions': self.transactions,
                  'nonce': nonce,
                  'previous_hash': previous_hash}
-        #reset the current list of transactions
-        self.transactions=[]
-        self.chain.append(block)
 
-# intansiate the class
+        # Reset the current list of transactions
+        self.transactions = []
+        self.chain.append(block)
+        return block
+
+    def verify_transaction_signature(self, sender_public_key, signature, transaction):
+        public_key = RSA.importKey(binascii.unhexlify(sender_public_key))
+        verifier = PKCS1_v1_5.new(public_key)
+        h = SHA.new(str(transaction).encode('utf8'))
+        try:
+            verifier.verify(h, binascii.unhexlify(signature))
+            return True
+        except ValueError:
+            return False
+
+    def submit_transaction(self, sender_public_key, recipient_public_key, signature, amount):
+        transaction = OrderedDict({
+            'sender_public_key': sender_public_key,
+            'recipient_public_key': recipient_public_key,
+            'amount': amount
+        })
+        if sender_public_key == MINING_SENDER:
+            self.transactions.append(transaction)
+            return len(self.chain) + 1
+        else:
+            signature_verification = self.verify_transaction_signature(sender_public_key, signature, transaction)
+            if signature_verification:
+                self.transactions.append(transaction)
+                return len(self.chain) + 1
+            else:
+                return False
+
+    @staticmethod
+    def valid_proof(transactions, last_hash, nonce, difficulty=MINING_DIFICULTY):
+        guess = (str(transactions) + str(last_hash) + str(nonce)).encode('utf8')
+        h = hashlib.new('sha256')
+        h.update(guess)
+        guess_hash = h.hexdigest()
+        return guess_hash[:difficulty] == '0' * difficulty
+
+    def proof_of_work(self):
+        nonce = 0
+        last_block = self.chain[-1]
+        last_hash = self.hash(last_block)
+        while self.valid_proof(self.transactions, last_hash, nonce) is False:
+            nonce += 1
+        return nonce
+
+    @staticmethod
+    def hash(block):
+        block_string = json.dumps(block, sort_keys=True).encode('utf8')
+        h = hashlib.new('sha256')
+        h.update(block_string)
+        return h.hexdigest()
+
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+        while current_index <len(chain):
+            block = chain[current_index]
+            self.hash(last_block)
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+            transactions= block['transactions'][:-1]
+            transaction_elements = ['sender_public_key', 'recipient_public_key', 'amount']
+            transactions = [OrderedDict((k, transaction[k]) for k in transaction_elements) for transaction in transactions]
+            if not self.valid_proof(transactions, block['previous_hash'], block['nonce'], MINING_DIFICULTY):
+                return False
+
+            last_block = block
+            current_index += 1
+        return True
+
+
+# Instantiate the Blockchain
 blockchain = Blockchain()
 
-#instantiate the node
+# Instantiate the Node
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route('/')
 def index():
     return render_template('./index.html')
 
-@app.route('/transaction/new', methods=['POST'])
-def new_transaction():
 
-    response = {'message' : 'OK'}
-    return jsonify(response), 201
+@app.route('/chain', methods=['GET'])
+def chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    nonce = blockchain.proof_of_work()
+    blockchain.submit_transaction(sender_public_key=MINING_SENDER, recipient_public_key=blockchain.node_id,
+                                  signature='', amount=MINING_REWARD)
+    last_block = blockchain.chain[-1]
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.create_block(nonce, previous_hash)
+    response = {'message': 'New Block Created',
+                'block_number': block['block_number'],
+                'transactions': block['transactions'],
+                'nonce': block['nonce'],
+                'previous_hash': block['previous_hash'],
+                }
+    return response, 200
+
+
+@app.route('/transactions/get', methods=['GET'])
+def get_transaction():
+    transactions = blockchain.transactions
+    response = {'transactions': transactions}
+    return jsonify(response), 200
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.form
+    required = ['confirmation_sender_public_key', 'confirmation_recipient_public_key', 'transaction_signature',
+                'confirmation_amount']
+    if not all(k in values for k in required):
+        return 'Missing Values', 400
+
+    transaction_results = blockchain.submit_transaction(values['confirmation_sender_public_key'],
+                                                        values['confirmation_recipient_public_key'],
+                                                        values['transaction_signature'], values['confirmation_amount'])
+    if transaction_results == False:
+        response = {'message': 'Invalid transaction/signature'}
+        return jsonify(response), 406
+    else:
+        response = {'message': 'Transaction will be added to the Block ' + str(transaction_results)}
+        return jsonify(response), 201
 
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_argument('-p','--port', default=5001, type=int, help="port to listen to")
+    parser.add_argument('-p', '--port', default=5001, type=int, help="port to listen to")
     args = parser.parse_args()
     port = args.port
 
-    app.run(host='127.0.0.1',port=port,debug=True)
+    app.run(host='127.0.0.1', port=port, debug=True)
